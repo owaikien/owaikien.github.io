1966a52bcc13530f18cef856d12aa196971f21de
Wai Kien <kienong2000@gmail.com>
2023-11-04 16:06:36 +0800

    dark_mode_edit

--- assets/js/button.js
+++ assets/js/button.js
@@ -1,16 +1,20 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
   const themeToggle = document.getElementById('theme-toggle');
 
   function toggleDarkTheme() {
-      document.body.classList.toggle('dark-theme');
-      const currentTheme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+      document.body.classList.toggle('dark-mode');
+      const currentTheme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
       localStorage.setItem('theme', currentTheme);
+      // Update button content based on the theme
+      themeToggle.textContent = currentTheme === 'dark' ? '☀️' : '🌙';
   }
 
   themeToggle.addEventListener('click', toggleDarkTheme);
 
-  const userThemePreference = localStorage.getItem('theme');
+  // Initialize theme on page load
+  const userThemePreference = localStorage.getItem('theme') || 'light'; // default to light theme
   if (userThemePreference === 'dark') {
-      document.body.classList.add('dark-theme');
+      document.body.classList.add('dark-mode');
+      themeToggle.textContent = '☀️'; // Update for dark mode
   }
 });
