1ec529009ed3234120c55131f022a241fb49cbf3
Wai Kien <kienong2000@gmail.com>
2023-11-04 12:53:45 +0800

    js_change

--- assets/js/button.js
+++ assets/js/button.js
@@ -1,23 +1,16 @@
-// Get the theme toggle button element
-const themeToggle = document.getElementById('theme-toggle');
+document.addEventListener('DOMContentLoaded', (event) => {
+  const themeToggle = document.getElementById('theme-toggle');
 
-// Function to toggle the dark theme and save preference
-function toggleDarkTheme() {
-  // Toggle the dark theme class on the body element
-  document.body.classList.toggle('dark-theme');
+  function toggleDarkTheme() {
+      document.body.classList.toggle('dark-theme');
+      const currentTheme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+      localStorage.setItem('theme', currentTheme);
+  }
 
-  // Save the user's theme preference to Local Storage
-  const currentTheme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
-  localStorage.setItem('theme', currentTheme);
-}
+  themeToggle.addEventListener('click', toggleDarkTheme);
 
-// Event listener for the theme toggle button
-themeToggle.addEventListener('click', toggleDarkTheme);
-
-// Check if the user's theme preference is already set
-const userThemePreference = localStorage.getItem('theme');
-
-// If the preference is set, apply the theme
-if (userThemePreference === 'dark') {
-  document.body.classList.add('dark-theme');
-}
+  const userThemePreference = localStorage.getItem('theme');
+  if (userThemePreference === 'dark') {
+      document.body.classList.add('dark-theme');
+  }
+});
